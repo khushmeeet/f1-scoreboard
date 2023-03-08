159ec11f2aee487509fb435261d3877c78cee604
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-03-07 22:57:03 -0500

    basic layout complete

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from datetime import datetime, timezone
 import ergast_py
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from filters import datetime_format
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+templates.env.globals['now'] = datetime.now(timezone.utc).astimezone()
+templates.env.filters["datetime_format"] = datetime_format
 e = ergast_py.Ergast()
 
 
