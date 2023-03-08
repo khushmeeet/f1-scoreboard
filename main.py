d44699c54c06b8af2fa0132a45b725b1286aad10
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-03-08 01:34:25 -0500

    Added status and pos change diff column

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from filters import datetime_format, laptime_format
+from filters import datetime_format, laptime_format, race_status_str
+
+# TODO:
+#   Add time interval
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
-templates.env.globals['now'] = datetime.now(timezone.utc).astimezone()
+templates.env.globals["now"] = datetime.now(timezone.utc).astimezone()
 templates.env.filters["datetime_format"] = datetime_format
 templates.env.filters["laptime_format"] = laptime_format
+templates.env.filters["race_status"] = race_status_str
+
 e = ergast_py.Ergast()
 
 
 @app.get("/", response_class=HTMLResponse)
 async def race_results(req: Request):
     data = e.season().round().get_results()
+    print(data)
     return templates.TemplateResponse(
         "race_results.html", {"request": req, "data": data[0]}
     )
