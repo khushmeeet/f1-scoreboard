27d664b60b56bb7d7032368257c8f6e1c03e366a
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-03-09 00:21:31 -0500

    Deployed to fly.io

---
+++ Procfile
@@ -0,0 +1,3 @@
+# Modify this Procfile to fit your needs
+
+web: uvicorn main:app --host 0.0.0.0 --port $PORT
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import json
 from datetime import datetime, timezone
 import ergast_py
-from fastapi.param_functions import Depends
-import httpx
 from fastapi import FastAPI, Request
+from fastapi.param_functions import Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from filters import datetime_format, laptime_format, race_status_str, combine_date_time
 from httpx_client import HTTPXClient
-from models import ScheduleModel
+from filters import datetime_format, laptime_format, race_status_str, combine_date_time
 from consts import RACE_SCHEDULE_URL
 from utils import get_schedule
 
@@ -55,7 +53,6 @@ async def race_schedule(req: Request, httpx_c=Depends(client)):
     schedule = await httpx_c.get(RACE_SCHEDULE_URL)
     schedule = json.loads(schedule.content)
     schedule = get_schedule(schedule)
-    print(schedule)
     return templates.TemplateResponse(
         "schedule.html", {"request": req, "schedule": schedule}
     )
